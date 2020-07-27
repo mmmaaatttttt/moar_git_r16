a81b45a45ad897f3bbe46b9a2c3a27eb1343b603
Matt Lane <lane.matthew@gmail.com>
2020-07-27 10:01:36 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function add(x=1, y=1) {
+function add(x = 0, y = 0) {
   return x + y;
 }
 
 function sayHi() {
   return "Hello";
+}
+
+function person1Fn() {
+  console.log("i'm person 1 building a feature");
+  return [1, 2, 3];
 }
