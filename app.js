018f7a050b3386e7dd009d9817ca99d52a514bfd
Matt Lane <lane.matthew@gmail.com>
2020-07-27 10:04:31 -0700

    add validation to add fn; add person2Fn

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-function add(x=1, y=1) {
+function add(x = 1, y = 0) {
+  if (typeof x !== "number" || typeof y !== "number")
+    throw new Error("Inputs must be numbers!");
   return x + y;
 }
 
 function sayHi() {
   return "Hello";
+}
+
+function person2Fn() {
+  console.log("hey this is a feature from person 2, isn't this great");
+  return "!";
 }
